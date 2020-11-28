4eb9f76302bd59041bbad743c3e537b76ddf6bcb
jdeburgo <jpdeburgo.jp@gmail.com>
2020-11-28 11:26:05 -0500

    initialized all screens

--- Project/StompCovidRumors/app/src/main/java/com/example/stompcovidrumors/NewPostActivity.kt
+++ Project/StompCovidRumors/app/src/main/java/com/example/stompcovidrumors/NewPostActivity.kt
@@ -1,16 +1,19 @@
 package com.example.stompcovidrumors
 
 import android.app.Activity
+import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Button
 import android.view.MenuItem
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import android.content.Intent
 
-class SearchActivity : Activity() {
+class NewPostActivity : Activity() {
     private lateinit var bottomNavigationView: BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.search)
+        setContentView(R.layout.new_post)
         bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
         bottomNavigationView.setOnNavigationItemSelectedListener { item -> bottomNavigationListener(item) }
     }
@@ -25,12 +28,12 @@ class SearchActivity : Activity() {
             }
             R.id.search -> {
                 // Respond to navigation item 2 click
+                val searchIntent = Intent(this, SearchActivity::class.java)
+                startActivity(searchIntent)
                 return true
             }
             R.id.new_post -> {
                 // Respond to navigation item 1 click
-                val postIntent = Intent(this, NewPostActivity::class.java)
-                startActivity(postIntent)
                 return true
             }
             R.id.profile -> {
